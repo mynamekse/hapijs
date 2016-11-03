496724ae503a084599bfbdc8b86e7f71733a8227
ihatecoding <iwebdd5@gmail.com>
2016-11-03 23:14:19 +0000

    First commit

---
+++ server.js
@@ -0,0 +1,69 @@
+/**
+ * Hapi.js server.
+ *
+ * @type {exports}
+ */
+
+var Hapi = require('hapi');
+var modules = require('./modules');
+
+// Instantiate the server
+var server = new Hapi.Server();
+
+server.connection({
+    port: 3000,
+    routes: {
+        cors: true,
+    }
+});
+
+
+/**
+ * The hapijs plugins that we want to use and their configs
+ */
+// TODO: Implement ability to add plugins via generator
+ var plugins = [
+//   {
+//     plugin: require('lout')
+//   }
+ ];
+
+/**
+ * Setup the server with plugins
+ */
+server.register(plugins, function(err) {
+
+  // If there is an error on server startup
+  if(err) {
+    throw err;
+  }
+
+  /**
+   * Make sure if this script is being required as a module by another script, we don't start the server.
+   */
+  if(!module.parent) {
+
+    /**
+     * Starts the server
+     */
+    server.start(function () {
+      console.log('Hapi server started @', server.info.uri);
+    });
+  }
+
+});
+
+/**
+ * Add all the modules within the modules folder
+ */
+for(var route in modules) {
+  server.route(modules[route]);
+}
+
+
+/**
+ * Expose the server's methods when used as a require statement
+ *
+ * @type {exports.server}
+ */
+module.exports = server;
